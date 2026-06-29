cd3a9e4752775bf87ead0433b0eb5e42555d5ba7
Joshua Thomas <134388878+Joshuathomas22135@users.noreply.github.com>
2026-06-29 19:43:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,46 @@
 three child classes (Dog, Parrot, Lion) that each inherit from Animal using super(), add their own unique attribute, and 
 implement speak() with their own output. Finally,  you create objects from each child class and run the sound show."""
 
-class Animal:
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
     def __init__(self, name):
         self.name = name
 
+    @abstractmethod
     def speak(self):
-        print(f"{self.name} is speaking")
+        pass
 
 class Dog(Animal):
     def __init__(self, age, name):
         self.age = age
         super().__init__(name)
 
+    def speak(self):
+        print(f"{self.name} is speaking.")
 
-d = Dog("Max", "4")
+class Parrot(Animal):
+    def __init__(self, age, name):
+        self.age = age
+        super().__init__(name)
+
+    def speak(self):
+        print(f"{self.name} is speaking.")
+
+class Lion(Animal):
+    def __init__(self, age, name):
+        self.age = age
+        super().__init__(name)
+
+    def speak(self):
+        print(f"{self.name} is speaking.")
+
+
+d = Dog("4", "Max")
 d.speak()
+
+p = Parrot("3", "Ruby")
+p.speak()
+
+l = Lion("4", "Mufasa")
+l.speak()
